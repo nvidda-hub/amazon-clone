57783d34fa1717fa53b9bf71e90f08461b5cf051
Narendra Vidda <narendra@monkey.travel>
2022-01-16 15:14:23 +0530

    navbar styling is done

---
+++ src/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img 
+                className='header_logo' 
+                src='https://pngimg.com/uploads/amazon/amazon_PNG25.png'
+            />
+            <div className='header_search'>
+                <input
+                    className='header_searchInput'
+                    type="text"
+                />
+                <SearchIcon 
+                    className="header_searchIcon" 
+                    />
+            </div>
+            <div className='header_nav'>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Hello Guest</span>
+                    <span className='header__optionLineTwo'>Sign in</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Returns</span>
+                    <span className='header__optionLineTwo'>Orders</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Your</span>
+                    <span className='header__optionLineTwo'>Prime</span>
+                </div>
+                <div className='header__optionBasket'>
+                    <ShoppingCartIcon />
+                    <span className='header__optionLineTwo header__basketCount'>0</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDl-pcou84jI_3zNVzrN4RXOeNxEm5LyVs",
+  authDomain: "clone-32ef4.firebaseapp.com",
+  projectId: "clone-32ef4",
+  storageBucket: "clone-32ef4.appspot.com",
+  messagingSenderId: "1023461593558",
+  appId: "1:1023461593558:web:75f8eb6de8be5aa96d5477",
+  measurementId: "G-4NN2CZ9MKY"
+};
