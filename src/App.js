e82acf0ba957b8544de6b8b4d7f8a8f22afe02c7
Narendra Vidda <narendra@monkey.travel>
2022-01-18 22:31:49 +0530

    getting user info in console

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 // import logo from './logo.svg';
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-
+import { auth } from "./firebase";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("the user : ", authUser);
+      if (authUser) {
+        // user is logged in or was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          // user is logged out
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="App">
-        <Header />
         <Routes>
-          <Route path="/" element={[<Home />]} />
+          <Route path="/" element={[<Header />, <Home />]} />
 
-          <Route path="/checkout" element={[<Checkout />]} />
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route path="/login" element={[<Login />]} />
         </Routes>
       </div>
     </Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDl-pcou84jI_3zNVzrN4RXOeNxEm5LyVs",
@@ -6,5 +8,12 @@ const firebaseConfig = {
   storageBucket: "clone-32ef4.appspot.com",
   messagingSenderId: "1023461593558",
   appId: "1:1023461593558:web:75f8eb6de8be5aa96d5477",
-  measurementId: "G-4NN2CZ9MKY"
+  measurementId: "G-4NN2CZ9MKY",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
